e89b3155d960d20ee53c20928027cee5f9e6f923
Fijxu <fijxu@zzls.xyz>
2021-08-18 20:30:40 -0400

    First commit with ♥

---
+++ games.js
@@ -0,0 +1,30 @@
+// juegos actualizados hasta el 18/08/2021  18:34 
+const games = [
+    {name: 'Otros', game_build_id: 0},
+    {name: 'Free Fire', game_build_id: 1},
+    {name: 'Fortnite', game_build_id: 5},
+    {name: 'Clash of clans', game_build_id: 25},
+    {name: 'League of Legends', game_build_id: 36},
+    {name: 'Overwatch', game_build_id: 37},
+    {name: 'Dota 2', game_build_id: 39},
+    {name: 'Apex', game_build_id: 40},
+    {name: 'Minecraft', game_build_id: 43},
+    {name: 'Brawl Stars', game_build_id: 46},
+    {name: 'CSGO', game_build_id: 48},
+    {name: 'COD: Black Ops 4', game_build_id: 49},
+    {name: 'Roblox', game_build_id: 74},
+    {name: 'GTA V', game_build_id: 77},
+    {name: 'Clash Royale', game_build_id: 105},
+    {name: 'Mario Kart', game_build_id: 110},
+    {name: 'Just Chatting', game_build_id: 100000},
+    {name: 'Among Us', game_build_id: 100001},
+    {name: 'FIFA 21', game_build_id: 100003},
+    {name: 'Fall Guys', game_build_id: 100005},
+    {name: 'Valorant', game_build_id: 100007},
+    {name: 'Call of Duty: Warzone', game_build_id: 100015},
+    {name: 'Mortal Kombat', game_build_id: 100021},
+    {name: 'Street fighther', game_build_id: 100022},
+    {name: 'The king of fighthers', game_build_id: 100023}
+]
+
+module.exports = games
---
+++ index.js
@@ -0,0 +1,168 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const colors = require('colors');
+
+const fetch = require('node-fetch');
+
+const sh = require('shelljs');
+
+const tmi = require('tmi.js');
+
+const twitchclient = new tmi.Client({
+    options: {
+        debug: true
+    },
+    identity: {
+        username: process.env.twitchusername,
+        password: process.env.twitchoauth
+    },
+    channels: [process.env.twitchchannels]
+});
+
+twitchclient.connect().catch('twitch client error: '+console.error);
+
+const Discord = require('discord.js');
+const {
+    MessageEmbed
+} = require('discord.js');
+const client = new Discord.Client({
+    intents: 36727
+});
+
+const games = require('./games');
+
+var isStreaming = false;
+var cooldown = false;
+var cooldownTimeout;
+
+const cooldownTime = 1000 * 60 * 60 * 2; // 2 hours
+const testConnectionCooldownTime = 3000; // 3 secounds
+const sessionKeyTest = 1000 * 60 * 60; // 1 hour
+
+
+console.log('Actual Cookies:'.cyan, process.env.cookies.yellow);
+console.log('session_key expire date:'.cyan, process.env.cookiedate.yellow);
+
+// When the discord bot is ready.
+client.on('ready', () => {
+    console.log(`Discord BOT Logged in as ${client.user.tag}!`);
+
+    // get the notifications channel of our discord server
+    const channel = client.channels.cache.find(channel => channel.id === process.env.discordchannel);
+
+    // Checks if "session_key" cookie is still valid
+    setInterval(function () {
+        // We use the v3 API of booyah for checking the stream status, but in this case is used for checking if
+        // "session_key" cookie is still valid.
+        // The responses may be "4030" if the session cookie is invalid or "404" if the streamer is offline.
+        fetch(`https://booyah.live/api/v3/channels/${process.env.userID}/streams`, {
+                'headers': {
+                    'accept': '*/*',
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'cookie': process.env.cookies,
+                },
+                'method': 'GET',
+            }).then(res => res.json())
+            .then(data => {
+                if (data.code === 4030) {
+                    console.log("The session key has expired, sending mail, discord private message and twitch whisper".red);
+                    client.users.cache.get(process.env.discorddm).send('The session key has expired')
+                    twitchclient.whisper(process.env.twitchdm, "The session key has expired")
+                        .then(function (data) {
+                            console.log('The twitch whisper has been sended', data);
+                        }).catch(function (err) {
+                            console.log('The twitch whisper failed to send: ', err);
+                        });
+                } else if (data.code === 404) {
+                    console.log("The session key is still valid".green);
+                }
+            });
+    }, sessionKeyTest);
+
+
+    // Test every "testConnectionCooldownTime" ms if the streamer if onfline or offline
+    setInterval(function () {
+        // We use the v3 API of booyah for checking the stream status
+        fetch(`https://booyah.live/api/v3/channels/${process.env.userID}/streams`, {
+                'headers': {
+                    'accept': '*/*',
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'cookie': process.env.cookies,
+                },
+                'method': 'GET',
+            }).then(res => res.json())
+            .then(data => {
+                if (data.code === 4030) {
+                    console.log("The session key has expired or it is invalid".red);
+                }
+
+                console.log('-----------------------'.random);
+                console.log('Request sent'.green);
+                console.log('Is streaming?: ', isStreaming);
+                console.log('Cooldown status: ', cooldown);
+                console.log('Viewer count:', data.viewer_count, typeof data.viewer_count);
+
+                
+
+                const discordNotificationEmbed = new MessageEmbed()
+                    .setColor('#ffff00')
+                    .setTitle(process.env.embedtitle)
+                    .setURL(`https://booyah.live/channels/${process.env.userID}`)
+                    .setAuthor(process.env.channelname, process.env.channellogo, `https://booyah.live/channels/${process.env.userID}`)
+                    .setDescription(process.env.embeddescription)
+                    .setThumbnail(process.env.embedthumbnail)
+                    .addFields({
+                            name: 'Game',
+                            value: `Just Chatting`,
+                            inline: true
+                        }, // game
+                        {
+                            name: 'Spectators',
+                            value: `${data.viewer_count}`,
+                            inline: true
+                        }, // viewers
+                    )
+                    .setImage(data.snapshot_webp)
+                    .setTimestamp()
+                    .setFooter(process.env.botname, process.env.botlogo);
+
+                // If the stream is online
+                if (typeof data.viewer_count === 'number' && !isStreaming) {
+                    isStreaming = true;
+                    cooldown = true;
+
+                    // Send Notification
+                    console.log('The stream is on'.green);
+                    channel.send({
+                        content: process.env.discordmessage,
+                        embeds: [discordNotificationEmbed]
+                    });
+
+                    twitchclient.say(process.env.twitchchannel, `${process.env.twitchmessage}, https://booyah.live/channels/${process.env.userID}`) // CABROS EL CRISTIAN PRENDIO EN BOOYAH! VAYAN A VERLO CTM!
+                        .then((data) => {
+                            console.log("The twitch notifications was sent.");
+                        }).catch((err) => {
+                            console.log("The twitch notification failed: ", err);
+                        });
+
+                    // We clear the cooldown if it is runing, and start a new cooldown
+                    clearTimeout(cooldownTimeout);
+
+                    cooldownTimeout = setTimeout(() => {
+                        console.log('Cooldown ended, waiting for the next stream'.yellow);
+                        cooldown = false;
+                    }, cooldownTime)
+
+                    // If the streamer is offline
+                } else if (typeof data.viewer_count === "undefined" && !cooldown) {
+                    isStreaming = false;
+                    console.log("The streamer isn't streaming".red, `\n`);
+                }
+
+            });
+    }, testConnectionCooldownTime);
+
+});
+
+client.login(process.env.discordbottoken);
